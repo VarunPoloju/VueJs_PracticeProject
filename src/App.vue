3f0de4a85ab7764b60c24068f143465d90813497
varun <varunpoloju966@gmail.com>
2024-05-02 09:01:44 +0530

    created repo and added App.vue file

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul></ul>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official guide of VueJs',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'Google site',
+                    link: 'https://google.co.in',
+                },
+            ],
+        };
+    },
+};
+</script>
